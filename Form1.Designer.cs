09fce1fd9a6a422702f84190b1cfcfa214691746
Hai Nguyen <hnguyen@itx.com>
2016-11-14 00:18:10 -0800

    test

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace itxSync
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.RichTextBox();
+			this.textBox2 = new System.Windows.Forms.RichTextBox();
+			this.browse1 = new System.Windows.Forms.Button();
+			this.browse2 = new System.Windows.Forms.Button();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 472);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Copy";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(12, 348);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox3.Size = new System.Drawing.Size(924, 106);
+			this.textBox3.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(115, 472);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Clear Logs";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(220, 472);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "Clear";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(13, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(482, 290);
+			this.textBox1.TabIndex = 6;
+			this.textBox1.Text = "";
+			this.textBox1.WordWrap = false;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(501, 12);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(435, 290);
+			this.textBox2.TabIndex = 7;
+			this.textBox2.Text = "";
+			this.textBox2.WordWrap = false;
+			// 
+			// browse1
+			// 
+			this.browse1.Location = new System.Drawing.Point(12, 308);
+			this.browse1.Name = "browse1";
+			this.browse1.Size = new System.Drawing.Size(75, 23);
+			this.browse1.TabIndex = 8;
+			this.browse1.Text = "Select File";
+			this.browse1.UseVisualStyleBackColor = true;
+			this.browse1.Click += new System.EventHandler(this.browse1_Click);
+			// 
+			// browse2
+			// 
+			this.browse2.Location = new System.Drawing.Point(501, 308);
+			this.browse2.Name = "browse2";
+			this.browse2.Size = new System.Drawing.Size(95, 23);
+			this.browse2.TabIndex = 9;
+			this.browse2.Text = "Select Folder";
+			this.browse2.UseVisualStyleBackColor = true;
+			this.browse2.Click += new System.EventHandler(this.browse2_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(323, 472);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.TabIndex = 10;
+			this.btnSave.Text = "Save";
+			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.ClientSize = new System.Drawing.Size(948, 507);
+			this.Controls.Add(this.btnSave);
+			this.Controls.Add(this.browse2);
+			this.Controls.Add(this.browse1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "itxSync";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.RichTextBox textBox1;
+		private System.Windows.Forms.RichTextBox textBox2;
+		private System.Windows.Forms.Button browse1;
+		private System.Windows.Forms.Button browse2;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button btnSave;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace itxSync
+{
+	public partial class Form1 : Form
+	{
+		public string settingFile;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			string path = Directory.GetCurrentDirectory();
+			settingFile = path + "\\itxSync.txt";
+
+			if (!File.Exists(settingFile))
+			{
+				File.Create(settingFile);
+			}
+			else
+			{
+				string[] lines = File.ReadAllLines(settingFile);
+				for (var i = 0; i < lines.Count(); i++)
+				{
+					var line = lines[i].Trim().Split('|');
+					textBox1.Text += line[0].Trim() + "\n";
+					textBox2.Text += line[1].Trim() + "\n";
+				}
+				
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			
+			var text1 = textBox1.Text.Trim().Split('\n');
+			var text2 = textBox2.Text.Trim().Split('\n');
+			
+			for (var i=0; i<text1.Count(); i++)
+			{
+				if (!string.IsNullOrEmpty(text1[i]) && !string.IsNullOrEmpty(text2[i]))
+				{
+
+					string logs = "";
+					string file1 = text1[i].Trim();
+					string file2 = text2[i].Trim();
+
+					try
+					{
+						var tmp = file1.Split('\\');
+						var fname = tmp[tmp.Length - 1];
+						file2 = file2 + "\\" + fname;
+
+						File.Copy(file1, file2, true);
+					}
+					catch(Exception ex)
+					{
+						logs = "ERROR - " + ex.Message + " - ";
+					}
+
+					logs += DateTime.Now.ToString("u").ToString().Replace("Z", "") + " - " + file1 + " --> " + file2 + "\r\n";
+					textBox3.Text = logs + textBox3.Text;
+
+				}
+			}
+
+		}
+		
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			textBox3.Text = "";
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "";
+			textBox2.Text = "";
+			textBox3.Text = "";
+		}
+
+		private void browse1_Click(object sender, EventArgs e)
+		{
+			
+			DialogResult result = openFileDialog1.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				textBox1.Text += openFileDialog1.FileName + "\n";
+			}
+			
+		}
+		private void browse2_Click(object sender, EventArgs e)
+		{
+			DialogResult result = folderBrowserDialog1.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				textBox2.Text += folderBrowserDialog1.SelectedPath + "\n";
+			}
+		}
+
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			var text1 = textBox1.Text.Trim().Split('\n');
+			var text2 = textBox2.Text.Trim().Split('\n');
+
+			string lines = "";
+			for (var i = 0; i < text1.Count(); i++)
+			{
+				if (!string.IsNullOrEmpty(text1[i].Trim()) && !string.IsNullOrEmpty(text2[i].Trim()))
+				{
+					lines += text1[i].Trim() + "|" + text2[i].Trim() + "\r\n";
+				}
+			}
+
+			File.WriteAllText(settingFile, lines);
+			textBox3.Text = "Saved";
+		}
+
+	}
+}
